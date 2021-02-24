913096782316f3edebf66bed8dba267fd0e3c10e
heriksuseno <heriksuseno21@mail.com>
2021-02-24 12:21:40 +0700

    Tugas 17 JS Untuk NIOMIC

---
+++ tugas17.js
@@ -0,0 +1,9 @@
+function kalimat() {
+  var A = 'Saya beLajar bahaSa peMrograman deNgan khUsuk.';
+  var B = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.';
+  console.log(A.toLowerCase());
+  console.log(A.toUpperCase());
+  console.log(B.toLowerCase());
+  console.log(B.toUpperCase());
+}
+kalimat()
